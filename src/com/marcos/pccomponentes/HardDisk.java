a01cf55e5921a992f1f8eccbd545b0357ee782cc
Marcos Elena San Segundo <marcos.elesan@educa.jcyl.es>
2022-10-17 18:37:56 +0200

    Creación de una clase para el disco duro

---
+++ src/com/marcos/pccomponentes/HardDisk.java
@@ -0,0 +1,45 @@
+package com.marcos.pccomponentes;
+
+public class HardDisk {
+
+    private Integer id;
+    private String marca;
+    private String capacidad;
+    private Integer precio;
+
+    /**
+     * Métodos, funciones, comportamiento
+     */
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getCapacidad(){
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad){
+        this.capacidad = capacidad;
+    }
+
+    public Integer getPrecio(){
+        return precio;
+    }
+
+    public void setPrecio(Integer precio){
+        this.precio = precio;
+    }
+
+}
